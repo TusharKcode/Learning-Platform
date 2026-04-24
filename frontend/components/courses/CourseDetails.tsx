b2919e841f93d1d181aa27d7127f11fe459e8e9a
Tushar Kumar <tkgoyal104@gmail.com>
2026-04-24 18:56:54 +0530

    Course Details Page UI Updated

--- frontend/components/courses/CourseDetails.tsx
+++ frontend/components/courses/CourseDetails.tsx
@@ -5,7 +5,7 @@ import {Button} from "@/components/ui/button"
 import Image from "next/image"
 import { Star, Users } from "lucide-react"
 import { useState } from "react"
-import CourseCarousel from "./CourseCarousel"
+// import CourseCarousel from "./CourseCarousel"
 
 type CourseDetailsType = {
     title: string
